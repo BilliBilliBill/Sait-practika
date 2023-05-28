ea247d1e794dfb2f33ecb084fe2c70e25ae6c031
Elena <puma12844@gmail.com>
2023-05-29 05:41:06 +0700

    Создан footer

--- src/pages/katalog/index.jsx
+++ src/pages/katalog/index.jsx
@@ -3,94 +3,206 @@ import './style.scss'
 const KatalogPage = () => {
   return (
     <div className="katalog">
-      <div className="katalogs_header">
+      <div className="kataloss_header">
         <div className="medium-text1">MarooN</div>
         <div className={'tovars'}>
           <div className={'tovar'}>
             <img className={'t1'} src={'/images/t1.png'}></img>
-            <div className="tovar-item-info">
-              <p className={'name_En'}>Sun</p>
-              <p className={'price'}>90pуб</p>
-              <p className={'name_Rus'}>бомбочка для ванны</p>
-              <p className={'V'}>20g</p>
+            <table>
+              <tr>
+                <th>Sun</th>
+                <th>90pуб</th>
+              </tr>
+              <tr>
+                <td>бомбочка для ванны</td>
+                <td>20g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
             </div>
           </div>
           <div className={'tovar'}>
             <img className={'t2'} src={'/images/t2.png'}></img>
-            <p className={'name_En'}>Levender</p>
-            <p className={'price'}>290pуб</p>
-            <p className={'name_Rus'}>мыло ручной работы</p>
-            <p className={'V'}>50g</p>
+            <table>
+              <tr>
+                <th>Levender</th>
+                <th>290pуб</th>
+              </tr>
+              <tr>
+                <td>мыло ручной работы</td>
+                <td>50g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t3'} src={'/images/t3.png'}></img>
-            <p className={'name_En'}>Lotos</p>
-            <p className={'price'}>890pуб</p>
-            <p className={'name_Rus'}>маска для лица</p>
-            <p className={'V'}>50ml</p>
+            <table>
+              <tr>
+                <th>Lotos</th>
+                <th>890pуб</th>
+              </tr>
+              <tr>
+                <td>маска для лица</td>
+                <td>50ml</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t4'} src={'/images/t4.png'}></img>
-            <p className={'name_En'}>Coconut</p>
-            <p className={'price'}>990pуб</p>
-            <p className={'name_Rus'}>масло для тела</p>
-            <p className={'V'}>300ml</p>
+            <table>
+              <tr>
+                <th>Coconut</th>
+                <th>990pуб</th>
+              </tr>
+              <tr>
+                <td>масло для тела</td>
+                <td>300ml</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t5'} src={'/images/t5.png'}></img>
-            <p className={'name_En'}>Clean</p>
-            <p className={'price'}>490pуб</p>
-            <p className={'name_Rus'}>маска для лица</p>
-            <p className={'V'}>100g</p>
+            <table>
+              <tr>
+                <th>Clean</th>
+                <th>490pуб</th>
+              </tr>
+              <tr>
+                <td>маска для лица</td>
+                <td>100g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t6'} src={'/images/t6.png'}></img>
-            <p className={'name_En'}>Violet</p>
-            <p className={'price'}>890pуб</p>
-            <p className={'name_Rus'}>крем для лица</p>
-            <p className={'V'}>50ml</p>
+            <table>
+              <tr>
+                <th>Violet</th>
+                <th>890pуб</th>
+              </tr>
+              <tr>
+                <td>крем для лица</td>
+                <td>50ml</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t7'} src={'/images/t7.png'}></img>
-            <p className={'name_En'}>Paradise</p>
-            <p className={'price'}>590pуб</p>
-            <p className={'name_Rus'}>минеральная пудра</p>
-            <p className={'V'}>15g</p>
+            <table>
+              <tr>
+                <th>Paradise</th>
+                <th>590pуб</th>
+              </tr>
+              <tr>
+                <td>минеральная пудра</td>
+                <td>15g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t8'} src={'/images/t8.png'}></img>
-            <p className={'name_En'}>Milk</p>
-            <p className={'price'}>790pуб</p>
-            <p className={'name_Rus'}>масло для тела</p>
-            <p className={'V'}>150ml</p>
+            <table>
+              <tr>
+                <th>Milk</th>
+                <th>790pуб</th>
+              </tr>
+              <tr>
+                <td>масло для тела</td>
+                <td>150ml</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t10'} src={'/images/t10.png'}></img>
-            <p className={'name_En'}>Rest</p>
-            <p className={'price'}>690pуб</p>
-            <p className={'name_Rus'}>минеральная пудра</p>
-            <p className={'V'}>20g</p>
+            <table>
+              <tr>
+                <th>Rest</th>
+                <th>690pуб</th>
+              </tr>
+              <tr>
+                <td>минеральная пудра</td>
+                <td>20g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t9'} src={'/images/t9.png'}></img>
-            <p className={'name_En'}>Rose</p>
-            <p className={'price'}>890pуб</p>
-            <p className={'name_Rus'}>крем для лица</p>
-            <p className={'V'}>50ml</p>
+            <table>
+              <tr>
+                <th>Rose</th>
+                <th>890pуб</th>
+              </tr>
+              <tr>
+                <td>крем для лица</td>
+                <td>50ml</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
           </div>
           <div className={'tovar'}>
             <img className={'t11'} src={'/images/t11.png'}></img>
-            <p className={'name_En'}>High</p>
-            <p className={'price'}>990pуб</p>
-            <p className={'name_Rus'}>крем для лица</p>
-            <p className={'V'}>50ml</p>
+            <table>
+              <tr>
+                <th>High</th>
+                <th>990pуб</th>
+              </tr>
+              <tr>
+                <td>крем для лица</td>
+                <td>50ml</td>
+              </tr>
+            </table>
+            <button className={'Podrob'}>Подробнее</button>
           </div>
           <div className={'tovar'}>
             <img className={'t12'} src={'/images/t12.png'}></img>
-            <p className={'name_En'}>Earth</p>
-            <p className={'price'}>90pуб</p>
-            <p className={'name_Rus'}>бомбочка для ванны</p>
-            <p className={'V'}>20g</p>
+            <table>
+              <tr>
+                <th>Earth</th>
+                <th>890pуб</th>
+              </tr>
+              <tr>
+                <td>бомбочка для ванны</td>
+                <td>20g</td>
+              </tr>
+            </table>
+            <div>
+              <button className={'Podrob'}>Подробнее</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img className={'PrisoedFon1'} src={'/images/60.png'}></img>
+          <div className="Pr11">Присоединяйтесь к нам</div>
+          <div className="Pr111">
+            Подпишитесь на наш аккаунт @marooncare<br></br> и узнавайте о
+            новиках и акциях первыми
           </div>
         </div>
       </div>
